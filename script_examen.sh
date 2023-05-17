3bfa72495c0b22e8550d68d3b04f16be022d8939
sciscar <90604890+sciscar@users.noreply.github.com>
2023-05-17 10:24:18 +0200

    Update script_examen.sh

--- script_examen.sh
+++ script_examen.sh
@@ -7,5 +7,6 @@ useradd -s /bin/bash -m examen
 echo examen:examen | chpasswd
 gpasswd -a examen docker
 su examen -c 'git clone https://github.com/sciscar/bd_examen.git && cd bd_examen && docker-compose up -d'
+#sleep 30
 #sh -c 'echo " pre-up iptables -A OUTPUT -p tcp -m owner --uid-owner examen -j DROP" >> /etc/network/interfaces'
-iptables -A OUTPUT -p tcp -m owner --uid-owner examen -j DROP
+#iptables -A OUTPUT -p tcp -m owner --uid-owner examen -j DROP
