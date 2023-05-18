b1e20a47c452e4ad5854b5aa1009cb496bb9170b
sciscar <90604890+sciscar@users.noreply.github.com>
2023-05-18 12:05:35 +0200

    pase lenllac directe

--- script_examen.sh
+++ script_examen.sh
@@ -6,7 +6,8 @@ groupadd docker
 useradd -s /bin/bash -m examen
 echo examen:examen | chpasswd
 gpasswd -a examen docker
+mv llanca_examen_bd/examen /home/examen/Escriptori/examen
 su examen -c 'cd /home/examen && git clone https://github.com/sciscar/bd_examen.git && cd bd_examen && docker-compose up -d'
 #sleep 30
 #sh -c 'echo " pre-up iptables -A OUTPUT -p tcp -m owner --uid-owner examen -j DROP" >> /etc/network/interfaces'
-#iptables -A OUTPUT -p tcp -m owner --uid-owner examen -j DROP
+iptables -A OUTPUT -p tcp -m owner --uid-owner examen -j DROP
