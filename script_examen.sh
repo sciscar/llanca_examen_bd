1b1c24625a39e038da9fc0675e1c2e45ef834bef
sciscar <90604890+sciscar@users.noreply.github.com>
2023-05-24 09:39:44 +0200

    Update script_examen.sh

--- script_examen.sh
+++ script_examen.sh
@@ -7,8 +7,11 @@ useradd -s /bin/bash -m examen
 echo examen:examen | chpasswd
 gpasswd -a examen docker
 mv llanca_examen_bd/lleva_conexio.sh /etc/init.d/
+git clone https://github.com/sciscar/bd_examen.git 
+cd bd_examen 
+docker-compose up -d
 #mv llanca_examen_bd/examen /home/examen/Escriptori/examen
-su examen -c 'cd /home/examen && git clone https://github.com/sciscar/bd_examen.git && cd bd_examen && docker-compose up -d'
+#su examen -c 'cd /home/examen && git clone https://github.com/sciscar/bd_examen.git && cd bd_examen && docker-compose up -d'
 #sleep 30
 #sh -c 'echo " pre-up iptables -A OUTPUT -p tcp -m owner --uid-owner examen -j DROP" >> /etc/network/interfaces'
 iptables -A OUTPUT -p tcp -m owner --uid-owner examen -j DROP
