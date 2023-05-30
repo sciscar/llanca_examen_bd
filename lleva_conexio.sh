8d8cc7a6824fc6cf8d28d4bb7602ed75c475b6bf
sciscar <90604890+sciscar@users.noreply.github.com>
2023-05-30 09:16:14 +0200

    Update lleva_conexio.sh

--- lleva_conexio.sh
+++ lleva_conexio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #iptables-restore /home/Administrador/llanca_examen_bd/regles_iptables.txt
-sudo iptables -A OUTPUT -p tcp -d aules.edu.gva.es --dport 443 -j ACCEPT
+iptables -A OUTPUT -p tcp -d aules.edu.gva.es --dport 443 -j ACCEPT
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -o wlan0 -p udp  --dport 53 -m state --state NEW -j ACCEPT
 iptables -A OUTPUT -o wlan0 -p tcp  --dport 53 -m state --state NEW -j ACCEPT
