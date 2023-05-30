93a474cc3e0211953bd04b10453c584ceed8e800
sciscar <90604890+sciscar@users.noreply.github.com>
2023-05-30 09:13:26 +0200

    iptables amb dominis

--- lleva_conexio.sh
+++ lleva_conexio.sh
@@ -1,1 +1,17 @@
-iptables-restore /home/Administrador/llanca_examen_bd/regles_iptables.txt
+#!/bin/bash
+#iptables-restore /home/Administrador/llanca_examen_bd/regles_iptables.txt
+sudo iptables -A OUTPUT -p tcp -d aules.edu.gva.es --dport 443 -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -o wlan0 -p udp  --dport 53 -m state --state NEW -j ACCEPT
+iptables -A OUTPUT -o wlan0 -p tcp  --dport 53 -m state --state NEW -j ACCEPT
+iptables -A OUTPUT -p tcp -d portal.edu.gva.es --dport 443 -j ACCEPT
+iptables -A OUTPUT -p tcp -d fonts.googleapis.com --dport 443 -j ACCEPT
+iptables -A OUTPUT -p tcp -d www.googletagmanager --dport 443 -j ACCEPT
+iptables -A OUTPUT -p tcp -d www.googletagmanager.com --dport 443 -j ACCEPT
+iptables -A OUTPUT -p tcp -d www.google-analytics.com --dport 443 -j ACCEPT
+iptables -A OUTPUT -p tcp -d cdnjs.cloudflare.com --dport 443 -j ACCEPT
+iptables -A OUTPUT -p tcp -d fonts.gstatic.com --dport 443 -j ACCEPT
+iptables -A OUTPUT -p tcp -d gva.es 443 -j ACCEPT
+iptables -A OUTPUT -p tcp -d gva.es --dport 443 -j ACCEPT
+iptables -A OUTPUT -p tcp -d cdn.jsdelivr.net --dport 443 -j ACCEPT
+iptables -A OUTPUT -o wlan0  -p tcp -m owner --uid-owner examen -j DROP
