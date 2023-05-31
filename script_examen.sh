82851f8ad742a257e52185ff2dab19e6d1263309
sciscar <90604890+sciscar@users.noreply.github.com>
2023-05-31 18:34:45 +0200

    Update script_examen.sh

--- script_examen.sh
+++ script_examen.sh
@@ -6,8 +6,9 @@ groupadd docker
 useradd -s /bin/bash -m examen
 echo examen:examen | chpasswd
 gpasswd -a examen docker
-echo "*/1 *   * * *   root    /bin/sh /home/Administrador/llanca_examen_bd/lleva_conexio.sh" >> /etc/crontab
-/etc/init.d/cron restart
+
 git clone https://github.com/sciscar/bd_examen.git 
 cd bd_examen 
 docker-compose up -d
+echo "*/2 *   * * *   root    /bin/sh /home/Administrador/llanca_examen_bd/lleva_conexio.sh" >> /etc/crontab
+/etc/init.d/cron restart
